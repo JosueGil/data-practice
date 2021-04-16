acdd56213a5c1e97409bfa96ada62092482d72a2
Josue Gil <josuegil82@gmail.com>
2021-04-15 23:46:43 -0700

    Testing code to automatically download data table.

--- code_test.R
+++ code_test.R
@@ -7,7 +7,6 @@ tab <- html_table(html_nodes(read_html(url),"table")[[1]],fill = TRUE) %>%
   filter(!state %in% c('2010')) %>% 
   mutate(population = as.numeric(str_replace_all(population,",|\\[\\d{1,2}\\]","")))
 
-
 library(tidyverse)
 library(gutenbergr)
 library(tidytext)
@@ -38,3 +37,48 @@ read(1)
 d <- mdy(dates)
 month(d)
 
+library(Lahman)
+library(ggplot2)
+library(tidyverse)
+t <- Teams %>% filter(yearID %in% 1961:2001) %>% 
+  mutate(rpg = R/G,bbpg = BB/G, hrpg = HR/G)
+lm(rpg~bbpg + hrpg,data = t)
+lm(rpg~hrpg,data = t)
+
+library(Lahman)
+bat_02 <- Batting %>% filter(yearID == 2002) %>%
+  mutate(pa = AB + BB, singles = (H - X2B - X3B - HR)/pa, bb = BB/pa) %>%
+  filter(pa >= 100) %>%
+  select(playerID, singles, bb)
+bat_03 <- Batting %>% filter(yearID %in% 1999:2001) %>%
+  mutate(pa = AB + BB, singles = (H - X2B - X3B - HR)/pa, bb = BB/pa) %>%
+  filter(pa >= 100) %>%
+  select(playerID, singles, bb) %>% group_by(playerID) %>% 
+  summarise(mean_singles = mean(singles),mean_bb = mean(bb)) %>% 
+  inner_join(bat_02, by = "playerID")
+cor(bat_03$bb,bat_03$mean_bb)
+bat_03 %>% ggplot(aes(mean_singles,singles)) +
+  geom_point()
+bat_03 %>% ggplot(aes(mean_bb,bb)) +
+  geom_point()
+lm(bb~mean_bb,data = bat_03)
+
+
+library(tidyverse)
+library(HistData)
+data("GaltonFamilies")
+set.seed(1) # if you are using R 3.5 or earlier
+set.seed(1, sample.kind = "Rounding") # if you are using R 3.6 or later
+galton <- GaltonFamilies %>%
+  group_by(family, gender) %>%
+  sample_n(1) %>%
+  ungroup() %>% 
+  gather(parent, parentHeight, father:mother) %>%
+  mutate(child = ifelse(gender == "female", "daughter", "son")) %>%
+  unite(pair, c("parent", "child"))
+ 
+galton %>% group_by(pair) %>% 
+  do(tidy(lm(childHeight~parentHeight, data = .),conf.int = TRUE))
+
+#<table id="vaccinations-table" class="expanded-data-table vaccinations-fontsize">
+  
