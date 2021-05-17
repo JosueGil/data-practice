ab99216f16ba175fa8554128c608e17fe35a23cf
Josue Gil <josuegil82@gmail.com>
2021-05-16 22:56:56 -0700

    More ML practice.

--- ML.R
+++ ML.R
@@ -186,3 +186,89 @@ dat %>% mutate(g = cut(x, quantile(x, ps), include.lowest = TRUE)) %>%
   group_by(g) %>%
   summarize(y = mean(y), x = mean(x)) %>%
   qplot(x,y,data = .)
+
+################################################
+#linear regression on self generated dataset 
+library(tidyverse)
+library(caret)
+
+# set.seed(1) # if using R 3.5 or earlier
+lin_model <- function(n){
+  Sigma <- 9*matrix(c(1.0, 0.5, 0.5, 1.0), 2, 2)
+  dat <- MASS::mvrnorm(n = n, c(69, 69), Sigma) %>%
+    data.frame() %>% setNames(c("x", "y"))
+  RMSE <- replicate(100,{
+    test_index <- createDataPartition(dat$y,times = 1, p = 0.5,list = FALSE)
+    train_set <- dat %>% slice(-test_index)
+    test_set <- dat %>% slice(test_index)
+    fit <- lm(y ~ x, data = train_set)
+    y_hat <- predict(fit, test_set)
+    sqrt(mean((y_hat - test_set$y)^2))
+  })
+  c(mean(RMSE),
+  sd(RMSE))
+}
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+sapply(c(100,500, 1000, 5000, 10000), lin_model)
+
+#Making correlation larger
+# set.seed(1) # if using R 3.5 or earlier
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+n <- 100
+Sigma <- 9*matrix(c(1.0, 0.95, 0.95, 1.0), 2, 2)
+dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+RMSE <- replicate(100,{
+  test_index <- createDataPartition(dat$y,times = 1, p = 0.5,list = FALSE)
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  fit <- lm(y ~ x, data = train_set)
+  y_hat <- predict(fit, test_set)
+  sqrt(mean((y_hat - test_set$y)^2))
+})
+c(mean(RMSE),
+  sd(RMSE))
+#Creating a dataset with two variables correlated to y
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.25, 0.75, 0.25, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+  data.frame() %>% setNames(c("y", "x_1", "x_2"))
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+test_index <- createDataPartition(dat$y,times = 1, p = 0.5,list = FALSE)
+train_set <- dat %>% slice(-test_index)
+test_set <- dat %>% slice(test_index)
+#prediction using only x_1
+fit <- lm(y ~ x_1, data = train_set)
+y_hat <- predict(fit, test_set)
+sqrt(mean((y_hat - test_set$y)^2))
+#prediction using only x_2
+fit <- lm(y ~ x_2, data = train_set)
+y_hat <- predict(fit, test_set)
+sqrt(mean((y_hat - test_set$y)^2))
+#prediction using x_1 and x_2
+fit <- lm(y ~ x_1 + x_2, data = train_set)
+y_hat <- predict(fit, test_set)
+sqrt(mean((y_hat - test_set$y)^2))
+
+#Creating a dataset where x_1 and x_2 are highly correlated
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.95, 0.75, 0.95, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+  data.frame() %>% setNames(c("y", "x_1", "x_2"))
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+test_index <- createDataPartition(dat$y,times = 1, p = 0.5,list = FALSE)
+train_set <- dat %>% slice(-test_index)
+test_set <- dat %>% slice(test_index)
+#prediction using only x_1
+fit <- lm(y ~ x_1, data = train_set)
+y_hat <- predict(fit, test_set)
+sqrt(mean((y_hat - test_set$y)^2))
+#prediction using only x_2
+fit <- lm(y ~ x_2, data = train_set)
+y_hat <- predict(fit, test_set)
+sqrt(mean((y_hat - test_set$y)^2))
+#prediction using x_1 and x_2
+fit <- lm(y ~ x_1 + x_2, data = train_set)
+y_hat <- predict(fit, test_set)
+sqrt(mean((y_hat - test_set$y)^2))
