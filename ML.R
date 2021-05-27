6566d1b3d7993ac551339978edfdd00ac2c0354b
Josue Gil <josuegil82@gmail.com>
2021-05-27 15:43:52 -0700

    More ml practice.

--- ML.R
+++ ML.R
@@ -272,3 +272,58 @@ sqrt(mean((y_hat - test_set$y)^2))
 fit <- lm(y ~ x_1 + x_2, data = train_set)
 y_hat <- predict(fit, test_set)
 sqrt(mean((y_hat - test_set$y)^2))
+
+################################
+#smoothing practice
+library(tidyverse)
+library(lubridate)
+library(purrr)
+library(pdftools)
+
+fn <- system.file("extdata", "RD-Mortality-Report_2015-18-180531.pdf", package="dslabs")
+dat <- map_df(str_split(pdf_text(fn), "\n"), function(s){
+  s <- str_trim(s)
+  header_index <- str_which(s, "2015")[1]
+  tmp <- str_split(s[header_index], "\\s+", simplify = TRUE)
+  month <- tmp[1]
+  header <- tmp[-1]
+  tail_index  <- str_which(s, "Total")
+  n <- str_count(s, "\\d+")
+  out <- c(1:header_index, which(n==1), which(n>=28), tail_index:length(s))
+  s[-out] %>%
+    str_remove_all("[^\\d\\s]") %>%
+    str_trim() %>%
+    str_split_fixed("\\s+", n = 6) %>%
+    .[,1:5] %>%
+    as_data_frame() %>% 
+    setNames(c("day", header)) %>%
+    mutate(month = month,
+           day = as.numeric(day)) %>%
+    gather(year, deaths, -c(day, month)) %>%
+    mutate(deaths = as.numeric(deaths))
+}) %>%
+  mutate(month = recode(month, "JAN" = 1, "FEB" = 2, "MAR" = 3, "APR" = 4, "MAY" = 5, "JUN" = 6, 
+                        "JUL" = 7, "AGO" = 8, "SEP" = 9, "OCT" = 10, "NOV" = 11, "DEC" = 12)) %>%
+  mutate(date = make_date(year, month, day)) %>%
+  dplyr::filter(date <= "2018-05-01")
+#smoothing using loess of puerto rico data
+dat <-  dat %>% filter(!is.na(deaths)) 
+span <- 60/1204
+fit <- loess(deaths ~ as.numeric(date), dat,span = span, degree = 1)
+
+dat  %>% mutate(smooth = fit$fitted) %>% 
+  ggplot(aes(date, deaths)) +
+  geom_point() + 
+  geom_line(aes(date, smooth),color="red")
+
+library(broom)
+library(dslabs)
+data(mnist_27)
+mnist_27$train %>% glm(y ~ x_2, family = "binomial", data = .) %>% tidy()
+qplot(x_2, y, data = mnist_27$train)
+
+mnist_27$train %>% 
+  mutate(y = ifelse(y=="7", 1, 0)) %>%
+  ggplot(aes(x_2, y)) + 
+  geom_smooth(method = "loess")
+
